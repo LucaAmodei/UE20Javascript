348f117c1bf56179e517e452a3d73ee5d912f923
Q210185 <l.amodei@student.helmo.be>
2024-06-08 10:15:08 +0200

    Extraction du js de fantasy

---
+++ PHPEmptyProject/4_Dom/fantasy/fantasy/fantasy.mjs
@@ -0,0 +1,34 @@
+const urlImages = ['fantasy-1.jpg', 'fantasy-2.jpg', 'fantasy-3.jpg', 'fantasy-4.jpg', 'fantasy-5.jpg']
+let count = 0;
+
+const vignettes = document.getElementById("vignettes")
+
+const add = document.getElementById("add")
+add.addEventListener('click', () => {
+    addPhoto()
+})
+
+addPhoto()
+
+function addPhoto() {
+    if (count === 10) {
+        add.style.display = "none"
+    } else {
+        addImage(urlImages[count % urlImages.length])
+        count++
+    }
+}
+
+function addImage(image) {
+    const img = document.createElement("img")
+    img.src = 'images/' + image
+    img.alt = "Image de " + image
+    img.classList.add('vignette')
+    img.addEventListener('click', () => {
+        const screen = document.getElementById("ecran")
+        screen.src = img.src
+        screen.alt = img.alt
+    })
+    img.click()
+    vignettes.appendChild(img)
+}
