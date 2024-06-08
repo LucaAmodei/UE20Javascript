108045f02ee1bd33b2ea293509702c54d5c0c419
LucaAmodei <145329170+LucaAmodei@users.noreply.github.com>
2024-06-08 18:38:51 +0200

    tentative de correction de l'ex nasa

--- PHPEmptyProject/6_AJAX/nasa/nasa.js
+++ PHPEmptyProject/6_AJAX/nasa/nasa.js
@@ -59,3 +59,55 @@ async function getPhoto() {
  * appelle une promesse qui va catch une autre promesse pour le mois d'après (gérer changement d'année dans les mois)
  *
  */
+/*
+Deuxieme essai (ca ne marche pas non plus jsp pq)
+const apiUrlphotos = `https://epic.gsfc.nasa.gov/api/enhanced/date/`
+
+let year = 2015
+let month = 0
+
+for (let i = 1; i <= 9; i++) {
+    getPhoto(i)
+}
+
+async function getPhoto(id) {
+    if (month === 12) {
+        month = 1
+        year++
+    } else {
+        month++
+    }
+
+    //Fin de la récursion
+    if (year === 2018 && month === 12) {
+        return null
+    }
+
+    month = month < 10 ? "0" + month : month
+    console.log(apiUrlphotos + `${year}-${month}-01`)
+    await fetch(apiUrlphotos + `${year}-${month}-01`).then(response => {
+        if (!response.ok) {
+            throw Error("Error lors de la réponse des photos")
+        }
+        return response
+    })
+        .then(response => response.json())
+        .then(result => {
+            if (result.length === 0) {
+                throw Error("Photo suivante")
+            }
+
+            const photo = result[Math.round(Math.random() * result.length)].image
+
+            let image = document.getElementById("nasa" + id)
+            image.innerHTML = ""
+            image.src = `https://epic.gsfc.nasa.gov/archive/enhanced/${year}/${month}/01/png/${photo}.png`;
+            image.alt = "nasa" + id + year + " " + month
+
+            document.getElementById("nbrP").innerText = id;
+        }).catch(error => {
+                getPhoto(id)
+            }
+        )
+}
+*/
