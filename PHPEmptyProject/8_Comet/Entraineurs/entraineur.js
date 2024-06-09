151a58cc042c2dbba906b7ae30e524ffdd3f36b2
Q210185 <l.amodei@student.helmo.be>
2024-06-09 11:25:06 +0200

    ajout

--- PHPEmptyProject/8_Comet/Entraineurs/entraineur.js
+++ PHPEmptyProject/8_Comet/Entraineurs/entraineur.js
@@ -1,3 +1,4 @@
+const url = "https://panoramix.cg.helmo.be/~p150107/tutoriels/js-comet/exercices/suite.php?n="
 const number = document.getElementById("number")
 const changeButton = document.getElementById("changeNumber")
 
@@ -20,6 +21,10 @@ function getRandomColors() {
 }
 
 function getNumber() {
-    return number.textContent + 1
-    //Utiliser le flux SSE a l'adresse : https://panoramix.cg.helmo.be/~p150107/tutoriels/js-comet/exercices/suite.php?n=x où x est un entier dont on veut des multiples
+    const value = document.getElementById("inputNumber").value
+    //TODO Here utiliser l'url pour récupérer les nombres qu'il faut
+    fetch(url + value).then()
+    return value !== "" ? value : number.textContent
 }
+
+//Cette exercice n'est pas fini mais je le fais pas vu que c'est pas la matière
