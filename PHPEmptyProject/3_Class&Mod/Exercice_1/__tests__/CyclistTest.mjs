ba45f9ac99db887b4a939203a7937468feb0b7a5
Q210185 <l.amodei@student.helmo.be>
2024-06-09 11:55:50 +0200

    Ajout de Cyclist.mjs

---
+++ PHPEmptyProject/3_Class&Mod/Exercice_1/__tests__/CyclistTest.mjs
@@ -0,0 +1,21 @@
+import assert from 'assert';
+import {TableVal} from "../../Th/tableval.mjs";
+
+describe('Cyclist', function(){
+    describe('somme', function(){
+        it('somme (1,2,3,4,-5) == 5', function(){
+            let t = new TableVal(1,2,3,4,-5);
+            assert.strictEqual(t.total, 5);
+        });
+        it('initialisation', function(){
+            let t = new TableVal(1,2,3,4,-5);
+            assert.deepEqual(t, {tab: [1,2,3,4,-5]});
+        });
+        it('isEmpty', function(){
+            let t = new TableVal(1,2,3,4,-5);
+            assert.strictEqual(t.isEmpty(), false, 'table t is not empty');
+            let v = new TableVal();
+            assert.ok(v.isEmpty(), 'table v is empty');
+        });
+    });
+});
